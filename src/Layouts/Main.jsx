f2a774f202bb6ad27e9e0ce2f2849414aa6aaf65
Anamul10 <anamul.hasan@mnsu.edu>
2023-07-08 14:28:26 -0500

    footer added

--- src/Layouts/Main.jsx
+++ src/Layouts/Main.jsx
@@ -1,10 +1,14 @@
 import { Outlet } from "react-router-dom";
+import Header from "../Components/Shared/Header";
+import Footer from "../Components/Shared/Footer";
 
 
 const Main = () => {
     return (
         <>
+            <Header />
             <Outlet />
+            <Footer />
         </>
     );
 };
