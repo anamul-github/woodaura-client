3998baf36231009fba6bb6e0e457685c8adf5bbe
Anamul10 <anamul.hasan@mnsu.edu>
2023-07-16 12:18:52 -0500

    header completed initially

--- src/Components/Shared/Header.jsx
+++ src/Components/Shared/Header.jsx
@@ -21,9 +21,9 @@ const Header = () => {
         <Disclosure as="nav" className="absolute top-0">
             {({ open }) => (
                 <>
-                    <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-                        <div className="relative flex h-16 items-center justify-between">
-
+                    <div className="mx-auto w-screen px-2 sm:px-6 lg:px-8">
+                        <div className="w-full relative flex h-16 items-center justify-between">
+                            {/* logo */}
                             <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                                 <div className="flex flex-shrink-0 items-center">
                                     <img
@@ -39,7 +39,8 @@ const Header = () => {
                                 </div>
 
                             </div>
-                            <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                            {/* routes  */}
+                            <div className="absolute inset-y-0 right-0 flex justify-end items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                                 <div className="hidden sm:ml-6 sm:block">
                                     <div className="flex space-x-4">
                                         {navigation.map((item) => (
@@ -60,6 +61,7 @@ const Header = () => {
 
 
                             </div>
+                            {/* mobile device  */}
                             <div className="absolute inset-y-0 right-0 flex items-center sm:hidden">
                                 {/* Mobile menu button*/}
                                 <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
@@ -76,7 +78,7 @@ const Header = () => {
 
                     {/* small device hamburger menu  */}
                     <Disclosure.Panel className="sm:hidden">
-                        <div className="space-y-1 px-2 pb-3 pt-2">
+                        <div className="bg-slate-900/50 backdrop-blur-md space-y-1 px-2 pb-3 pt-2">
                             {navigation.map((item) => (
                                 <Disclosure.Button
                                     key={item.name}
