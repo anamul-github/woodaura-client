1c0aededd1d38bc484a6ab3f0ab11734c05899eb
Anamul10 <anamul.hasan@mnsu.edu>
2023-10-07 12:41:02 -0500

    feat:services added but only one applied

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,6 @@
 import Categories from "../Components/Home/Categories/Categories";
 import Hero from "../Components/Home/Hero";
+import Services from "../Components/Home/Services/Services";
 import Testimonials from "../Components/Home/Testimonials/Testimonials";
 
 const Home = () => {
@@ -8,6 +9,7 @@ const Home = () => {
             <Hero />
             <Categories />
             <Testimonials />
+            <Services />
         </>
     );
 };
