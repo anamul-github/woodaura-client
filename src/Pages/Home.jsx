bb88b7e51f79722c9dd7b01f2fede49fa4a7a030
Anamul10 <anamul.hasan@mnsu.edu>
2023-10-07 12:33:26 -0500

    feat: Testimonials section added

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,13 @@
 import Categories from "../Components/Home/Categories/Categories";
 import Hero from "../Components/Home/Hero";
+import Testimonials from "../Components/Home/Testimonials/Testimonials";
 
 const Home = () => {
     return (
         <>
             <Hero />
             <Categories />
+            <Testimonials />
         </>
     );
 };
